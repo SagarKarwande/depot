439b824a1eddd3d6924f3a9ffb95b626b9a07071
Sagar Karwande <sagar_karwande123@yahoo.com>
2019-01-23 15:14:59 +0530

    handle product count in line item

---
+++ db/migrate/20190123092414_conbine_items_in_cart.rb
@@ -0,0 +1,35 @@
+class ConbineItemsInCart < ActiveRecord::Migration[5.0]
+  def up
+    Cart.all.each do |cart|
+      # count the number of product in cart
+      sums = cart.line_items.group(:product_id).sum(:quantity)
+
+      sums.each do |product_id, quantity|
+        # remove individual items
+        cart.line_items.where(product_id: product_id).delete_all
+
+        # replace with a single item
+        item = cart.line_items.build(product_id: product_id)
+        item.quantity = quantity
+        item.save!
+      end
+    end
+  end
+
+  def down
+    # split line items with quantity>1 into multiple items
+    LineItem.where('quantity>1').each do |line_item|
+      # add individual items
+      line_item.quantity.times do
+        LineItem.create(
+          cart_id: line_item.cart_id,
+          product_id: line_item.product_id,
+          quantity: 1
+        )
+      end
+
+      # remove orignal item
+      line_item.destroy
+    end
+  end
+end
